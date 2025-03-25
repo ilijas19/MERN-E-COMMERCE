7a1abf2d3dc75885880a35084322681075b71131
ilijas19 <ilijagocic19@gmail.com>
2025-03-25 20:45:20 +0100

    tsconig

---
+++ frontend/src/global.d.ts
@@ -0,0 +1,22 @@
+// Declare 'react-slick' module
+declare module "react-slick" {
+  const content: any;
+  export default content;
+}
+
+// Declare 'react-dom/client'
+declare module "react-dom/client" {
+  export * from "react-dom";
+}
+
+// Declare '@tailwindcss/vite'
+declare module "@tailwindcss/vite" {
+  const content: any;
+  export default content;
+}
+
+// Declare '@vitejs/plugin-react'
+declare module "@vitejs/plugin-react" {
+  const content: any;
+  export default content;
+}
